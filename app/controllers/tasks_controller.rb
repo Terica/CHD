5a637787de70a01cd7692e88e55c557810cff5f5
Terica <terica.douglas@gmail.com>
2019-05-21 00:38:38 -0700

    refactored Description to Body Project model; styles applied; Lato; Font Awesome

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,33 @@
 class TasksController < ApplicationController
   before_action :set_project
+  before_action :set_task, except: [:create]
+
   def create
     @task = @project.tasks.create(task_params)
     redirect_to @project
+  end
+
+  def destroy
+    if @task.destroy
+      flash[:success] = "Task was deleted"
+    else
+      flash[:error] = "Task was not deleted"
+    end
+  end
+
+  def complete
+    @task.update_attribute(:completed_at, Time.now)
+    redirect_to @project, notice: "Task Completed"
   end
 
   private
 
   def set_project
     @project = Project.find(params[:project_id])
+  end
+
+  def set_task
+    @task = @project.tasks.find(params[:project_id])
   end
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :projects do
-    resources :tasks
+    resources :tasks do
+      member do
+        patch :complete
+      end
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "projects#index"
 end
